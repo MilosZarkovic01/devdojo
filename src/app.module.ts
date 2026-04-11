2aff0a36419a493ac95b900ddbfbaa23b9117717
Milos Zarkovic <milos.zarkovic@sportserve.co>
2026-04-11 18:07:50 +0200

    Create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        `.env.${process.env.NODE_ENV}`,
+        '.env'
+      ]
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('POSTGRES_HOST'),
+        port: config.get<number>('POSTGRES_PORT'),
+        database: config.get<string>('POSTGRES_DB'),
+        username: config.get<string>('POSTGRES_USERNAME'),
+        password: config.get<string>('POSTGRES_PASSWORD'),
+        entities: [User],
+        synchronize: true
+      })
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ name: 'first_name' })
+    firstName: string;
+
+    @Column({ name: 'last_name' })
+    lastName: string;
+
+    @Column({ name: 'email' })
+    email: string;
+
+    @Column({ name: 'password' })
+    password: string;
+}
